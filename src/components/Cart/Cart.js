56c7c8fbc8284025d16800a9e8001f6a398c78dc
Rakesh <rakesh@s2bnexgens-MacBook-Pro.local>
2020-06-29 02:14:41 +0530

    paypal integrated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,6 @@ import Title from "../Title";
 import CartColumns from "./CartColumns";
 import EmptyCart from "./EmptyCart";
 import CartTotals from "./CartTotals";
-import styled from "styled-components";
 import { ProductConsumer } from "../../context";
 import CartList from "./CartList";
 export default class Cart extends Component {
@@ -19,7 +18,7 @@ export default class Cart extends Component {
                   <Title name="your" title="cart" />
                   <CartColumns />
                   <CartList value={value} />
-                  <CartTotals value={value} />
+                  <CartTotals value={value} history={this.props.history} />
                 </>
               );
             } else {
